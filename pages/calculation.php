5ee151ba2885dd01352da914bee1adb5db0de1f5
izotop <izotop84@gmail.com>
2025-12-16 20:05:19 +0200

    add pages folder

---
+++ pages/calculation.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'inc/global/_top.php'; ?>
+    <title>Kapitel :: Розрахунок</title>
+</head>
+
+<body>
+    <div id="content-block">
+        <?php
+        $item_class_1 = $item_class_2 = $item_class_3 = $item_class_4 = $item_class_5 = $item_class_6 = 'filled';
+        $item_class_7 = 'current';
+        include 'inc/global/_header.php';
+        ?>
+
+        <main>
+            <?php include 'template-parts/_banner-form.php'; ?>
+        </main>
+    </div>
+
+    <!-- Popups -->
+    <div class="popup-wrapper" id="popups"></div>
+
+    <?php include 'inc/global/_bottom.php'; ?>
+    <?php include 'inc/global/_form.php'; ?>
+
+</body>
+
+</html>
