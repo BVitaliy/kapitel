fb9e8e02e85a5308a6b761aa3bf25fb58a7dff82
izotop <izotop84@gmail.com>
2025-12-17 16:13:49 +0200

    responsive and other fixes

--- inc/global/_top.php
+++ inc/global/_top.php
@@ -2,7 +2,7 @@
 <meta name="description" content="Put your description here.">
 <meta name="format-detection" content="telephone=no" />
 <meta name="mobile-web-app-capable" content="yes">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta name="theme-color" content="var(--clr-white)" />
 
 <link rel="shortcut icon" href="img/favicon.ico" />
